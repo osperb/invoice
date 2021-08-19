9a0ea805ab1016512cb95bdf2163b2a6446c397d
fasilpaloli <palolifasil@gmail.com>
2021-08-19 11:53:42 +0530

    Updates

---
+++ precache-manifest.0502dbf959c5cd1691dbf09b0151e0b0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e7c6ba47a948951458ee20dc5669eb3",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "13a22a981d131473c37a",
+    "url": "/invoice/static/css/2.0d77bc6e.chunk.css"
+  },
+  {
+    "revision": "602dc829593425cd2061",
+    "url": "/invoice/static/css/main.da18699e.chunk.css"
+  },
+  {
+    "revision": "13a22a981d131473c37a",
+    "url": "/invoice/static/js/2.1296cba7.chunk.js"
+  },
+  {
+    "revision": "c5f92507b76b3fbc3f8e5f5fe0c84cf3",
+    "url": "/invoice/static/js/2.1296cba7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "602dc829593425cd2061",
+    "url": "/invoice/static/js/main.64438367.chunk.js"
+  },
+  {
+    "revision": "5fb982e8c8615729aefc",
+    "url": "/invoice/static/js/runtime-main.69bc0f1b.js"
+  },
+  {
+    "revision": "5af81432dda1dde2def604f8052f424a",
+    "url": "/invoice/static/media/download.5af81432.svg"
+  },
+  {
+    "revision": "e631f0ca65031b20fe23668e166aa85b",
+    "url": "/invoice/static/media/remove.e631f0ca.svg"
+  },
+  {
+    "revision": "a700735566a3701072afbd164b80e690",
+    "url": "/invoice/static/media/resize.a7007355.svg"
+  }
+]);
